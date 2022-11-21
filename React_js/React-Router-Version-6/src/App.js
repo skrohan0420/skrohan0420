75435da21c07971385e5fc96e5d3419a1aa3466c
skrohan0420 <skrohan0420@gmail.com>
2022-11-21 18:05:41 +0530

    react-router-6

---
+++ React_js/React-Router-Version-6/src/App.js
@@ -0,0 +1,51 @@
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import { useState } from 'react'
+
+function App() {
+
+	const [cartIsEmpty] = useState(true)
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<nav>
+					<h1>The Ninja Clothing Company</h1>
+					<Link to="/">Home</Link>
+					<Link to="/about">About</Link>
+					<Link to="/products">Products</Link>
+				</nav>
+				<Routes>
+					<Route path="/" element={<Home/>} />
+					<Route path="/about/*" element={<About />}/>
+					<Route path="/products/:id/*" element={<ProductDetails />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/test" element={(
+						<div>
+							<h2>Test Page</h2>
+							<p>Hello</p>
+						</div>
+					)} />
+					<Route path='/redirect' element={<Navigate to='/about' />} />
+					<Route 
+						path='/checkout'
+						element={
+							cartIsEmpty 
+							? 
+							<Navigate to='/products' />
+							:
+							<p>Checkout</p>	
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
+
+export default App
