53d3749d0c9446426d49200ab7ade9a2cfd88702
skrohan0420 <skrohan0420@gmail.com>
2022-12-06 19:22:33 +0530

    react redux comp

--- React_js/react-redux-saga-project/src/redux/action.js
+++
@@ -1,9 +0,0 @@
-import { ADD_TO_CART,REMOVE_TO_CART } from "./constant"
-
-export const addToCart = (data) => {
-    console.log(data)
-    return{
-        type : ADD_TO_CART,
-        data : data 
-    }
-}
--- React_js/react-redux-saga-project/src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { cartData } from "./reducer";
-
-export default combineReducers({
-    cartData,
-})
